52c0c2860c650d6be44e5f2729942bd61c1774fc
RomasCosta <romariocosta25@outlook.com>
2024-02-16 17:57:06 -0300

    add testes na aplicação

--- src/main/java/com/github/costa/romario/daycode/controller/FilmeController.java
+++ src/main/java/com/github/costa/romario/daycode/controller/FilmeController.java
@@ -4,19 +4,34 @@
  */
 package com.github.costa.romario.daycode.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
  *
  * @author romar
+ * https://developer.themoviedb.org/docs/authentication-application
  */
 @RestController
 public class FilmeController {
     
-    private RestTemplate restTemplate;
+    private final String chaveAPI = "7e5bfe5a363366719815d747052b151b";
+    private final String urlTMDB = "https://api.themoviedb.org/3/discover/movie";
+    
+    private final RestTemplate restTemplate;
 
     public FilmeController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+    
+    @GetMapping("/top20")
+    public String getTop20Movies(){
+        
+        ResponseEntity<String> response =
+                this.restTemplate.getForEntity(urlTMDB + "?api_key=" + chaveAPI , String.class);
+
+        return response.getBody();
     }
 }
--- src/test/java/com/github/costa/romario/daycode/ApplicationTests.java
+++ src/test/java/com/github/costa/romario/daycode/ApplicationTests.java
@@ -1,13 +1,32 @@
 package com.github.costa.romario.daycode;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
+    
+    @LocalServerPort
+    private int getPort;
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @Test
+    void shouldReturnTop20Films() {
+
+        ResponseEntity<String> response
+                = this.restTemplate.getForEntity("http://localhost:" + getPort + "/top20", String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        
+    }
 }
